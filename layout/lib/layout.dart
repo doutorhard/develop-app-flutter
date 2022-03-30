c355c7cf4116e9530360383f22505c2147a604c9
doutorhard <doutorhard@hotmail.com>
2022-03-30 09:37:15 -0300

    Criação do menu de navegação

--- layout/lib/layout.dart
+++ layout/lib/layout.dart
@@ -14,9 +14,75 @@ class LayoutState extends State<Layout> {
         appBar: AppBar(
           title: const Text("Layout - Flutter"),
         ),
-        drawer: const Drawer(
-            //
-            ),
+
+        // Menu Lateral (hamburger)
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              // Cabeçalho do Menu
+              DrawerHeader(
+                  child: Text('Itens do Menu'),
+                  decoration: BoxDecoration(color: Colors.blueAccent)),
+
+              // Item 1
+              ListTile(
+                title: const Text("Flutter"),
+                subtitle: Text("Tudo São Widgets"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                leading: Icon(
+                  Icons.flutter_dash,
+                  color: Colors.blue,
+                  size: 32,
+                ),
+                // onTap: () {
+                // Aqui vai o codigo de um método
+                // Navigator.pop(context);
+                // }
+              ),
+
+              // Item 2
+              ListTile(
+                title: const Text("Dart"),
+                subtitle: Text("Coração Flutter"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                leading: Icon(Icons.heart_broken_rounded,
+                    color: Colors.red, size: 32),
+                // onTap: () {
+                // Aqui vai o codigo de um método
+                // Navigator.pop(context);
+                // }
+              ),
+
+              // Item 3
+              ListTile(
+                title: const Text("Cafessínio"),
+                subtitle: Text("Não vivo sem!"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                leading: Icon(Icons.coffee, color: Colors.brown, size: 32),
+
+                // onTap: () {
+                // Aqui vai o codigo de um método
+                // Navigator.pop(context);
+                // }
+              ),
+
+              // Item 4
+              ListTile(
+                title: const Text("Ajustes"),
+                subtitle: Text("Configurações do Sistema"),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                leading: Icon(Icons.settings, color: Colors.teal, size: 32),
+
+                // onTap: () {
+                // Aqui vai o codigo de um método
+                // Navigator.pop(context);
+                // }
+              )
+            ],
+          ),
+        ),
+
         // Corpo do App
         body: const Center(child: Text('Layout Básico')),
 
@@ -24,16 +90,14 @@ class LayoutState extends State<Layout> {
         bottomNavigationBar: BottomNavigationBar(
           items: [
             // ícone Home
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
 
             // ícone Configurações
-            const BottomNavigationBarItem(
+            BottomNavigationBarItem(
                 icon: Icon(Icons.settings), label: 'Ajustes'),
 
             // ícone login
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.login), label: 'Login'),
+            BottomNavigationBarItem(icon: Icon(Icons.login), label: 'Login')
           ],
         ),
       ),
